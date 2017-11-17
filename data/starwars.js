fd7651de03b56864ccc8666a8087f71352fd5f38
Jonathan M Nguyen <31453488+jonathanMNg@users.noreply.github.com>
2017-11-16 19:26:42 -1000

    updates movie

--- data/starwars.js
+++ data/starwars.js
@@ -2,13 +2,22 @@ var actors = [
   {
     "first": "mark",
     "last": "hamill",
+    "movies": [
+      "star_wars4",
+    ]
   },
   {
     "first": "harrison",
     "last": "ford",
+    "movies": [
+      "star_wars4",
+    ]
   },
   {
     "first": "carrie",
     "last": "fisher",
+    "movies": [
+      "star_wars4",
+    ]
   },
 ];
