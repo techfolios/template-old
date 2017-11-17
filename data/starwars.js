41133db7bf62c8dffaaf528a408bcade378f1213
Jonathan M Nguyen <31453488+jonathanMNg@users.noreply.github.com>
2017-11-16 19:02:35 -1000

    starwars data
    test 1

---
+++ data/starwars.js
@@ -0,0 +1,13 @@
+var actors = [
+  {
+    "first": "mark",
+    "last": "hamill",
+  },
+  {
+    "first": "harrison",
+    "last": "ford",
+  },
+  {
+    "first": "carrie",
+    "last": "fisher",
+  },
