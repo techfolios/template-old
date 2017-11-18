385a9cb8e8763c96615e0fcacc1a748f4362c694
Jonathan M Nguyen <31453488+jonathanMNg@users.noreply.github.com>
2017-11-17 17:59:49 -1000

    initialize startek data

---
+++ data/startrek.js
@@ -0,0 +1,442 @@
+var s_actors = [
+  {
+    "first": "william",
+    "last": "shatner",
+    "movies": [
+      "star_trek1",
+      "star_trek2",
+      "star_trek3",
+      "star_trek4",
+      "star_trek5",
+      "star_trek6",
+    ]
+  },
+  {
+    "first": "leonard",
+    "last": "nimoy",
+    "movies": [
+      "star_trek1",
+      "star_trek2",
+      "star_trek3",
+      "star_trek4",
+      "star_trek5",
+      "star_trek6",
+      "star_trek11",
+    ]
+  },
+  {
+    "first": "deforest",
+    "last": "kelley",
+    "movies": [
+      "star_trek1",
+      "star_trek2",
+      "star_trek3",
+      "star_trek4",
+      "star_trek5",
+      "star_trek6",
+    ]
+  },
+  {
+    "first": "james",
+    "last": "doohan",
+    "movies": [
+      "star_trek1",
+      "star_trek2",
+      "star_trek3",
+      "star_trek4",
+      "star_trek5",
+      "star_trek6",
+      "star_trek7",
+    ]
+  },
+  {
+    "first": "george",
+    "last": "takei",
+    "movies": [
+      "star_trek1",
+      "star_trek2",
+      "star_trek3",
+      "star_trek4",
+      "star_trek5",
+      "star_trek6",
+    ]
+  },
+  {
+    "first": "majel",
+    "last": "barrett",
+    "movies": [
+      "star_trek1",
+    ]
+  },
+  {
+    "first": "walter",
+    "last": "koenig",
+    "movies": [
+      "star_trek1",
+      "star_trek2",
+      "star_trek3",
+      "star_trek4",
+      "star_trek5",
+      "star_trek6",
+      "star_trek7",
+    ]
+  },
+  {
+    "first": "nichelle",
+    "last": "nichols",
+    "movies": [
+      "star_trek1",
+      "star_trek2",
+      "star_trek3",
+      "star_trek4",
+      "star_trek5",
+      "star_trek6",
+    ]
+  },
+  {
+    "first": "persis",
+    "last": "khambatta",
+    "movies": [
+      "star_trek1",
+    ]
+  },
+  {
+    "first": "stephen",
+    "last": "collins",
+    "movies": [
+      "star_trek1",
+    ]
+  },
+  {
+    "first": "bibi",
+    "last": "besch",
+    "movies": [
+      "star_trek2",
+    ]
+  },
+  {
+    "first": "merritt",
+    "last": "butrick",
+    "movies": [
+      "star_trek2",
+      "star_trek3",
+    ]
+  },
+  {
+    "first": "paul",
+    "last": "winfield",
+    "movies": [
+      "star_trek2",
+    ]
+  },
+  {
+    "first": "phil",
+    "last": "morris",
+    "movies": [
+      "star_trek3",
+    ]
+  },
+  {
+    "first": "jane",
+    "last": "wyatt",
+    "movies": [
+      "star_trek4",
+    ]
+  },
+  {
+    "first": "catherine",
+    "last": "hicks",
+    "movies": [
+      "star_trek4",
+    ]
+  },
+  {
+    "first": "mark",
+    "last": "lenard",
+    "movies": [
+      "star_trek4",
+      "star_trek6",
+    ]
+  },
+  {
+    "first": "david",
+    "last": "warner",
+    "movies": [
+      "star_trek5",
+    ]
+  },
+  {
+    "first": "laurence",
+    "last": "luckinbill",
+    "movies": [
+      "star_trek5",
+    ]
+  },
+  {
+    "first": "charles",
+    "last": "cooper",
+    "movies": [
+      "star_trek5",
+    ]
+  },
+  {
+    "first": "kim",
+    "last": "cattrall",
+    "movies": [
+      "star_trek6",
+    ]
+  },
+  {
+    "first": "leon",
+    "last": "russom",
+    "movies": [
+      "star_trek6",
+    ]
+  },
+  {
+    "first": "patrick",
+    "last": "stewart",
+    "movies": [
+      "star_trek7",
+      "star_trek8",
+      "star_trek9",
+      "star_trek10",
+    ]
+  },
+  {
+    "first": "jonathan",
+    "last": "frakes",
+    "movies": [
+      "star_trek7",
+      "star_trek8",
+      "star_trek9",
+      "star_trek10",
+    ]
+  },
+  {
+    "first": "brent",
+    "last": "spiner",
+    "movies": [
+      "star_trek7",
+      "star_trek8",
+      "star_trek9",
+      "star_trek10",
+    ]
+  },
+  {
+    "first": "levar",
+    "last": "burton",
+    "movies": [
+      "star_trek7",
+      "star_trek8",
+      "star_trek9",
+      "star_trek10",
+    ]
+  },
+  {
+    "first": "michael",
+    "last": "dorn",
+    "movies": [
+      "star_trek7",
+      "star_trek8",
+      "star_trek9",
+      "star_trek10",
+    ]
+  },
+  {
+    "first": "gates",
+    "last": "mcfadden",
+    "movies": [
+      "star_trek7",
+      "star_trek8",
+      "star_trek9",
+      "star_trek10",
+    ]
+  },
+  {
+    "first": "marina",
+    "last": "sirtis",
+    "movies": [
+      "star_trek7",
+      "star_trek8",
+      "star_trek9",
+      "star_trek10",
+    ]
+  },
+  {
+    "first": "malcoml",
+    "last": "mcdowell",
+    "movies": [
+      "star_trek7",
+    ]
+  },
+  {
+    "first": "alfre",
+    "last": "woodard",
+    "movies": [
+      "star_trek8",
+    ]
+  },
+  {
+    "first": "james",
+    "last": "cromwell",
+    "movies": [
+      "star_trek8",
+    ]
+  },
+  {
+    "first": "alice",
+    "last": "krige",
+    "movies": [
+      "star_trek8",
+    ]
+  },
+  {
+    "first": "murray",
+    "last": "abraham",
+    "movies": [
+      "star_trek8",
+    ]
+  },
+  {
+    "first": "donna",
+    "last": "murphy",
+    "movies": [
+      "star_trek8",
+    ]
+  },
+  {
+    "first": "anthony",
+    "last": "zerbe",
+    "movies": [
+      "star_trek8",
+    ]
+  },
+  {
+    "first": "tom",
+    "last": "hardy",
+    "movies": [
+      "star_trek10",
+    ]
+  },
+  {
+    "first": "ron",
+    "last": "perlman",
+    "movies": [
+      "star_trek10",
+    ]
+  },
+  {
+    "first": "shannon",
+    "last": "cochran",
+    "movies": [
+      "star_trek10",
+    ]
+  },
+  {
+    "first": "chris",
+    "last": "pine",
+    "movies": [
+      "star_trek11",
+      "star_trek12",
+      "star_trek13",
+    ]
+  },
+  {
+    "first": "zachary",
+    "last": "quinto",
+    "movies": [
+      "star_trek11",
+      "star_trek12",
+      "star_trek13",
+    ]
+  },
+  {
+    "first": "bruce",
+    "last": "greenwood",
+    "movies": [
+      "star_trek11",
+      "star_trek12",
+    ]
+  },
+  {
+    "first": "karl",
+    "last": "urban",
+    "movies": [
+      "star_trek11",
+      "star_trek12",
+      "star_trek13",
+    ]
+  },
+  {
+    "first": "zoe",
+    "last": "saldana",
+    "movies": [
+      "star_trek11",
+      "star_trek12",
+      "star_trek13",
+    ]
+  },
+  {
+    "first": "simon",
+    "last": "pegg",
+    "movies": [
+      "star_trek11",
+      "star_trek12",
+      "star_trek13",
+    ]
+  },
+  {
+    "first": "john",
+    "last": "cho",
+    "movies": [
+      "star_trek11",
+      "star_trek12",
+      "star_trek13",
+    ]
+  },
+  {
+    "first": "anton",
+    "last": "yelchin",
+    "movies": [
+      "star_trek11",
+      "star_trek12",
+      "star_trek13",
+    ]
+  },
+  {
+    "first": "benedict",
+    "last": "cumberbatch",
+    "movies": [
+      "star_trek12",
+    ]
+  },
+  {
+    "first": "peter",
+    "last": "weller",
+    "movies": [
+      "star_trek12",
+    ]
+  },
+  {
+    "first": "idris",
+    "last": "elba",
+    "movies": [
+      "star_trek13",
+    ]
+  },
+  {
+    "first": "sofia",
+    "last": "boutella",
+    "movies": [
+      "star_trek13",
+    ]
+  },
+  {
+    "first": "joe",
+    "last": "taslim",
+    "movies": [
+      "star_trek13",
+    ]
+  },
+];
