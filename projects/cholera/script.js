df6336e9605dd72928696895c81ec62a0c3acfe6
rnaka <rae.loves@gmail.com>
2020-09-26 22:38:13 -1000

    Update script.js

--- projects/cholera/script.js
+++ projects/cholera/script.js
@@ -304,21 +304,14 @@ var censusLayout = {
 Plotly.newPlot('pieDiv2', censusData, censusLayout);
 
 
+var mymap = L.map('mapid').setView([51.5135, -0.136], 17);
 
-
-
-
-
-
-var mymap = L.map('mapid').setView([0,0],0);
-L.tileLayer('https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token=pk.eyJ1IjoibWFwYm94IiwiYSI6ImNpejY4NXVycTA2emYycXBndHRqcmZ3N3gifQ.rJcFIG214AriISLbB6B5aw', {
+L.tileLayer('https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}', {
+    attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
     maxZoom: 18,
-    attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, ' +
-        '<a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, ' +
-        'Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
     id: 'mapbox/streets-v11',
     tileSize: 512,
-    zoomOffset: -1
+    zoomOffset: -1,
+    accessToken: 'pk.eyJ1IjoieHJlaWx5bm54IiwiYSI6ImNrZmt1N2dnbjE0bGUyeXBrY2NnNmhyc2MifQ.RrIAbVQGGRP9zyTi-cYvlA'
 }).addTo(mymap);
-
 
