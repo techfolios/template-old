b7a38fba85ba785cd37f63c64895142de1bad5d8
Rutherford-sudo <lucas.a@aluno.ufop.edu.br>
2021-01-27 20:11:58 -0300

    Filter Option in Essays

--- js/filter.js
+++ js/filter.js
@@ -1,6 +1,6 @@
 var $btns = $('.filterButton').click(function () {
     if (this.id == 'all'){
-        $('#parent2 > div').fadeIn(450);
+        $('#parent2 > div').fadeIn("slow");
     }else{
         var $el = $('.' + this.id).fadeIn("slow");
         $('#parent2 > div').not($el).fadeOut("fast", function(){
@@ -10,3 +10,10 @@ var $btns = $('.filterButton').click(function () {
     $btns.removeClass('active');
     $(this).addClass('active');
 })
+
+$(window).on('resize', function () {
+    var win = $(this);
+    if (win.width() < 600){
+        $('#listaProjetos').removeClass('center').addClass('left')
+    }
+});
